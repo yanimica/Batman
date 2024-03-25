7c9ecbe4207213b87045452d548f6aeca5ae4d0a
Yanina Martin <yaninamicamartin@gmail.com>
2024-03-25 19:42:37 -0300

    se agrega cambio de img

--- linterna.js
+++ linterna.js
@@ -4,17 +4,19 @@ let bati_boton = document.querySelector("#bati_boton");
 let encendido = document.querySelector("#batiEncendido");
 let batman = document.querySelector("#batman");
 let click = document.querySelector("#batiClick");
+batman.classList.add("batman_inactivo");
+
 
 
 const controlar_linterna = () => {
-    click
-        ? (estadoLinterna === "false")
-            ? (estadoLinterna = "true", batman.classList.add("batman_activo"))
-            : (estadoLinterna = "false", batman.classList.remove("batman_activo"))
-        : null;
+    estadoLinterna = !estadoLinterna;
+    if (estadoLinterna) {
+        batman.style.backgroundImage = 'url("./img/batmanON.gif")';
+    } else {
+        batman.style.backgroundImage = 'url("./img/batmanOFF.jpg")';
+    }
     sonido();
 }
-
 
 bati_boton.addEventListener("click", controlar_linterna)
 
