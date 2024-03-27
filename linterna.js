88e99bef4fe7957205d608285af6e7ae0384d4c0
Yanina Martin <yaninamicamartin@gmail.com>
2024-03-27 15:05:46 -0300

    cambio en img

--- linterna.js
+++ linterna.js
@@ -8,14 +8,26 @@ batman.classList.add("batman_inactivo");
 
 
 
-const controlar_linterna = () => {
+/* const controlar_linterna = () => {
 
     if (estadoLinterna) {
         batman.style.backgroundImage = 'url("./img/batmanON.gif")';
+        sonido();
     } else {
         batman.style.backgroundImage = 'url("./img/batmanOFF.jpg")';
     }
-    sonido();
+} */
+
+const controlar_linterna = () => {
+    if (estadoLinterna) {
+        batman.style.backgroundImage = 'url("./img/batmanON.gif")';
+        sonido(true); // Se pasa true para indicar que la linterna se está encendiendo
+        estadoLinterna = false; // Se actualiza el estado de la linterna
+    } else {
+        batman.style.backgroundImage = 'url("./img/batmanOFF.jpg")';
+        sonido(true); // Se pasa false para indicar que la linterna se está apagando
+        estadoLinterna = true; // Se actualiza el estado de la linterna
+    }
 }
 
 
@@ -28,6 +40,7 @@ function sonido() {
     } else {
         click.play();
         encendido.pause();
+        encendido.currentTime = 0;
     }
 }
 
